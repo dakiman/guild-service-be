b8a57c022bc938c76e478a7cbba6521668e5fc94
Daki <dvancov@hotmail.com>
2020-03-11 23:08:20 +0100

    performance test

--- tests/Feature/RandoTest.php
+++ tests/Feature/RandoTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class RandoTest extends TestCase
@@ -12,9 +13,56 @@ class RandoTest extends TestCase
     /** @test */
     public function rando()
     {
-//        $response = $this->get('/api/character/The%20Maelstrom/Snorlâx?locale=eu');
-//
-//        $response->assertStatus(200);
+        $client = new Client([
+            'base_uri' => 'https://guild-service-be.herokuapp.com/api/'
+        ]);
+
+        $guilds = [
+            ['name' => 'complexity-limit', 'locale' => 'us', 'realm' => 'illidan'],
+            ['name' => 'method', 'locale' => 'eu', 'realm' => 'tarren-mill'],
+            ['name' => 'Aversion', 'locale' => 'eu', 'realm' => 'blackhand'],
+        ];
+
+        foreach ($guilds as $guild) {
+            $response = $client->get($this->getUrlForGuild($guild['realm'], $guild['name'], $guild['locale']));
+            $roster = json_decode($response->getBody())->guild->roster;
+
+            $counter = 1;
+            $chunks = collect($roster)->chunk(5);
+
+            foreach ($chunks as $chunk) {
+                Log::info("\n Now doing request no $counter for guild " . $guild['name']);
+                $counter++;
+
+                $urls = [];
+                foreach($chunk as $member) {
+                    array_push($urls, $this->getUrlForCharacter($member->realm, $member->name, $member->region));
+                }
+
+                $promises = [];
+                foreach($urls as $url) {
+                    array_push($promises, $client->getAsync($url));
+                }
+
+                try {
+                    Promise\unwrap($promises);
+                } catch (\Exception $e) {
+                    Log::info("\n Exception  happened when unwrapping promises");
+                }
+
+                Log::info("================================");
+            }
+        }
+    }
+
+    private function getUrlForGuild($realm, $name, $locale)
+    {
+        return "guild/" . $realm . "/" . $name . "?locale=" . $locale;
+    }
+
+    private function getUrlForCharacter($realm, $name, $locale)
+    {
+        return "character/" . $realm . "/" . $name . "?locale=" . $locale;
     }
 
 }
