cd9a58eabfb30761cd68047adcbf05b4f74e155e
dakiman <dvancov@hotmail.com>
2020-11-02 00:39:02 +0100

    user_id nullable

--- app/DTO/Character/CharacterDocument.php
+++ app/DTO/Character/CharacterDocument.php
@@ -11,7 +11,7 @@ class CharacterDocument extends FlexibleDataTransferObject
     public string $name;
     public string $realm;
     public string $region;
-    public string $user_id;
+    public ?string $user_id;
     public ?int $num_of_searches;
     public ?bool $recruitment;
     public BlizzardData $blizzard_data;
