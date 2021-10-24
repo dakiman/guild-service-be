85f9d9f628e9052ab9d3a3f0723d60692ac2f96e
dakiman <dvancov@hotmail.com>
2021-10-24 20:34:13 +0200

    change user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'name' => 'Daki',
-            'email' => 'daki@daki.com'
+//        User::factory()->create([
+//            'name' => 'Daki',
+//            'email' => 'daki@daki.com'
+//        ]);
+
+        User::create([
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'name' => "Daki",
+            'email' => 'daki@daki',
+            'password' => bcrypt('password'),
         ]);
     }
 }
